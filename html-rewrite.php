5d2f631c618f3efcb2f8aa9ca205f29b250201a3
HassanCh <hasaan.ch.13@gmail.com>
2025-06-11 18:58:18 +0500

    Add functions to skip processing for various page builders and editors

--- html-rewrite.php
+++ html-rewrite.php
@@ -11,8 +11,76 @@ function flying_scripts_is_keyword_included($content, $keywords)
     return false;
 }
 
+function flying_scripts_should_skip_processing() {
+    // Skip if in admin area
+    if (is_admin()) {
+        return true;
+    }
+    
+    // Skip for Elementor edit/preview mode
+    if (isset($_GET['elementor-preview']) || 
+        isset($_GET['elementor_library']) || 
+        isset($_GET['action']) && $_GET['action'] === 'elementor') {
+        return true;
+    }
+    
+    // Skip if Elementor is in edit mode (check for Elementor constants/functions)
+    if (defined('ELEMENTOR_VERSION')) {
+        // Check if we're in Elementor edit mode
+        if (isset($_GET['action']) && $_GET['action'] === 'elementor') {
+            return true;
+        }
+        
+        // Check for Elementor preview
+        if (isset($_GET['elementor-preview'])) {
+            return true;
+        }
+        
+        // Additional check for Elementor edit mode
+        if (function_exists('\Elementor\Plugin::instance') && 
+            \Elementor\Plugin::instance()->editor && 
+            \Elementor\Plugin::instance()->editor->is_edit_mode()) {
+            return true;
+        }
+        
+        // Check for Elementor preview mode
+        if (function_exists('\Elementor\Plugin::instance') && 
+            \Elementor\Plugin::instance()->preview && 
+            \Elementor\Plugin::instance()->preview->is_preview_mode()) {
+            return true;
+        }
+    }
+    
+    // Skip for other page builders that might have similar issues
+    // Beaver Builder
+    if (isset($_GET['fl_builder']) || 
+        (function_exists('FLBuilderModel') && FLBuilderModel::is_builder_active())) {
+        return true;
+    }
+    
+    // Divi Builder
+    if (function_exists('et_core_is_fb_enabled') && et_core_is_fb_enabled()) {
+        return true;
+    }
+    
+    // Gutenberg editor (Block editor)
+    if (function_exists('is_admin') && 
+        (strpos($_SERVER['REQUEST_URI'], 'post.php') !== false || 
+         strpos($_SERVER['REQUEST_URI'], 'post-new.php') !== false) &&
+        isset($_GET['action']) && $_GET['action'] === 'edit') {
+        return true;
+    }
+    
+    return false;
+}
+
 function flying_scripts_rewrite_html($html)
 {
+    // Skip processing if we should not process
+    if (flying_scripts_should_skip_processing()) {
+        return $html;
+    }
+    
     // Process only GET requests
     if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
         return $html;
@@ -55,6 +123,12 @@ function flying_scripts_rewrite_html($html)
             $script->getAttribute('id') === 'flying-scripts') {
             continue;
         }
+        
+        // Skip Elementor scripts to prevent breaking the editor
+        if (strpos($script_text, 'elementor') !== false) {
+            continue;
+        }
+        
         if (flying_scripts_is_keyword_included($script_text, $include_list)) {
             if (flying_scripts_is_keyword_included($script_text, $jquery_dependent)) {
                 $script->setAttribute("data-type", "jquery-lazy");
@@ -81,12 +155,15 @@ function flying_scripts_rewrite_html($html)
     return $newHtml;
 }
 
-if (!is_admin()) {
+// Modified condition to use the new skip function
+if (!flying_scripts_should_skip_processing()) {
     ob_start("flying_scripts_rewrite_html");
 }
 
 // W3TC HTML rewrite
 add_filter('w3tc_process_content', function ($buffer) {
-    if (is_admin()) return $buffer;
+    if (flying_scripts_should_skip_processing()) {
+        return $buffer;
+    }
     return flying_scripts_rewrite_html($buffer);
 });
--- inject-js.php
+++ inject-js.php
@@ -1,5 +1,65 @@
 <?php
+function flying_scripts_should_skip_js_injection() {
+    // Skip if in admin area
+    if (is_admin()) {
+        return true;
+    }
+    
+    // Skip for Elementor edit/preview mode
+    if (isset($_GET['elementor-preview']) || 
+        isset($_GET['elementor_library']) || 
+        isset($_GET['action']) && $_GET['action'] === 'elementor') {
+        return true;
+    }
+    
+    // Skip if Elementor is in edit mode (check for Elementor constants/functions)
+    if (defined('ELEMENTOR_VERSION')) {
+        // Check if we're in Elementor edit mode
+        if (isset($_GET['action']) && $_GET['action'] === 'elementor') {
+            return true;
+        }
+        
+        // Check for Elementor preview
+        if (isset($_GET['elementor-preview'])) {
+            return true;
+        }
+        
+        // Additional check for Elementor edit mode
+        if (function_exists('\Elementor\Plugin::instance') && 
+            \Elementor\Plugin::instance()->editor && 
+            \Elementor\Plugin::instance()->editor->is_edit_mode()) {
+            return true;
+        }
+        
+        // Check for Elementor preview mode
+        if (function_exists('\Elementor\Plugin::instance') && 
+            \Elementor\Plugin::instance()->preview && 
+            \Elementor\Plugin::instance()->preview->is_preview_mode()) {
+            return true;
+        }
+    }
+    
+    // Skip for other page builders
+    // Beaver Builder
+    if (isset($_GET['fl_builder']) || 
+        (function_exists('FLBuilderModel') && FLBuilderModel::is_builder_active())) {
+        return true;
+    }
+    
+    // Divi Builder
+    if (function_exists('et_core_is_fb_enabled') && et_core_is_fb_enabled()) {
+        return true;
+    }
+    
+    return false;
+}
+
 function flying_scripts_inject_js() {
+    // Don't inject JS if we should skip processing
+    if (flying_scripts_should_skip_js_injection()) {
+        return;
+    }
+    
     $timeout = intval(get_option('flying_scripts_timeout', 5));
     $first_visit_only = get_option('flying_scripts_first_visit_only', 0) ? true : false;
     ?>
